126a9789e1defa930bafb4aa206ee5cd5178d429
Tiago Pimentel <tiagopms@gmail.com>
2021-05-05 18:16:26 +0100

    Add extra dependency run for hpc

--- hpc/sbatch_all.sh
+++ hpc/sbatch_all.sh
@@ -4,4 +4,6 @@ do
     # Get embeddings
     jid2=$(LANGUAGE=${lang} sbatch slurm/hpc.wilkes2)
     jid2=$(echo $jid2 | cut -d' ' -f4)
+    echo "Submitted " ${lang} ${jid2}
+    LANGUAGE=${lang} sbatch --dependency=afternotok:${jid2} slurm/hpc.wilkes2
 done
